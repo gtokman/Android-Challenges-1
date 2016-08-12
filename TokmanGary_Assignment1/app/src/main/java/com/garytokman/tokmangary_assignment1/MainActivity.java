0366ac5ca2fe9c9ec2a4d5cc105af1c1d29aadfe
gtokman <gtokman@fullsail.edu>
2016-08-12 11:56:51 -0400

    Initial commit for assignment 1

---
+++ TokmanGary_Assignment1/app/src/main/java/com/garytokman/tokmangary_assignment1/MainActivity.java
@@ -0,0 +1,24 @@
+package com.garytokman.tokmangary_assignment1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Spinner mSpinner;
+    private TextView mName;
+    private TextView mAge;
+    private ImageView mProfileImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Init
+        
+    }
+}

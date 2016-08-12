3082630f2d1221806037230a0003ad6d04a2830e
gtokman <gtokman@fullsail.edu>
2016-08-11 20:11:49 -0400

    final commit lab5

--- TokmanGary_CE06/app/src/main/java/com/garytokman/tokmangary_ce06/model/People.java
+++ TokmanGary_CE06/app/src/main/java/com/garytokman/tokmangary_ce06/model/People.java
@@ -14,9 +14,9 @@ public class People {
         List<Person> people = new ArrayList<>();
 
         people.add(new Person("Bob", "Smith", "11/07/94", R.drawable.person1));
-        people.add(new Person("Sam", "Johnson", "11/07/95", R.drawable.person2));
-        people.add(new Person("Ryan", "Gorton", "11/07/64", R.drawable.person3));
-        people.add(new Person("Joe", "Dimmie", "11/07/84", R.drawable.person4));
+        people.add(new Person("Sam", "John", "11/07/95", R.drawable.person2));
+        people.add(new Person("Ryan", "Gort", "11/07/64", R.drawable.person3));
+        people.add(new Person("Joe", "Dim", "11/07/84", R.drawable.person4));
         people.add(new Person("Rex", "Jorn", "11/07/84", R.drawable.person5));
         people.add(new Person("Tom", "Jordan", "11/07/94", R.drawable.person6));
         people.add(new Person("Jake", "Bryant", "11/07/95", R.drawable.person7));
